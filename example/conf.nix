1bff836fab4969b2be6e2fb3e0c7326fb8a2718b
Dustin Lacewell <dlacewell@gmail.com>
2020-05-25 14:56:17 -0500

    Update example/site.nix to 0.8 spec

--- example/conf.nix
+++ example/conf.nix
@@ -1,14 +1,15 @@
+{ pkgs ? import <nixpkgs> { }, lib }:
+
 {
-  /* URL of the site, must be set to the url of the domain the site will be deployed
-  */
+  # URL of the site, must be set to the url of the domain the site will be deployed
   siteUrl = "https://styx-static.github.io/styx-theme-generic-templates";
 
   /* Theme specific settings
      it is possible to override any of the used themes configuration in this set
   */
   theme = {
-    lib.bootstrap.enable    = true;
-    lib.jquery.enable       = true;
+    lib.bootstrap.enable = true;
+    lib.jquery.enable = true;
     lib.font-awesome.enable = true;
   };
 
